7f02fb17c532683ccf4e63c69203336ce8514ad2
Jerald-Jn <jjerald2000@gmail.com>
2025-10-20 17:49:20 +0530

    fix some responsive

--- src/components/Educations/EducationCard.tsx
+++ src/components/Educations/EducationCard.tsx
@@ -1,6 +1,6 @@
 export function EducationCard(data: any) {
     return (
-        <div className="flex flex-col bg-black/20 gap-2 p-5 rounded-3xl">
+        <div className="flex flex-col bg-black/20 gap-2 p-5 rounded-3xl mx-3">
             <h3 className="text-black dark:text-neutral-200 text-center text-2xl font-medium md:font-semibold">{data.data.Course}</h3>
             <div className="flex flex-col justify-start">
                 {Object.entries(data.data).map(([key, value]) => {
--- src/components/Educations/EducationsSection.tsx
+++ src/components/Educations/EducationsSection.tsx
@@ -3,7 +3,7 @@ import { EducationCard } from "./EducationCard";
 
 export function EducationsSection() {
     return (
-        <section id="educations" className="lg:mx-10 p-4 md:p-10 my-5 text-black dark:text-neutral-100">
+        <section id="educations" className=" pt-5 text-black dark:text-neutral-100">
             <h2 className="text-center text-3xl font-semibold tracking-wider pb-2 uppercase">Educations</h2>
             <div className={`my-8 grid grid-cols-1 gap-8 md:my-12 md:grid-cols-2 lg:mx-52`}>
                 {educations.map((education) => (
