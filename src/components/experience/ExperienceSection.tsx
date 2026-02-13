a3d2c1c12c9d8994d5c4e607eb1701f7f1e5c252
Jerald-Jn <jjerald2000@gmail.com>
2026-02-13 22:47:06 +0530

    Update ExperienceSection

--- src/components/experience/ExperienceSection.tsx
+++ src/components/experience/ExperienceSection.tsx
@@ -9,7 +9,7 @@ const ExperienceSection = (data:any) => {
                 <h3 className=" text-3xl font-bold text-center mt-3 lg:mt-5">Software Engineer</h3>
                 <div className=" grid grid-cols-1 text-black dark:text-neutral-200">
                         <h4 className="text-neutral text-lg font-bold">Trackeasy Software Solutions</h4>
-                        <p className="text-neutral text-md font-semibold">January-2025 - Present</p>
+                        <p className="text-neutral text-md font-semibold">January-2025 - January-2026</p>
                         <p className="text-neutral text-md font-semibold">Thanjavur, TamilNadu, India</p>
                 </div>
             </div>
