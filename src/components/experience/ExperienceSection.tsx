d614c254e3318a64660c3848838d98079c880f7d
Jerald-Jn <jjerald2000@gmail.com>
2026-02-13 22:45:17 +0530

    Update experience titile

--- src/components/experience/ExperienceSection.tsx
+++ src/components/experience/ExperienceSection.tsx
@@ -6,7 +6,7 @@ const ExperienceSection = (data:any) => {
             >
             <h2 className="text-center text-3xl font-semibold tracking-wider uppercase">EXperience</h2>
             <div className="flex flex-col justify-center items-center">
-                <h3 className=" text-3xl font-bold text-center mt-3 lg:mt-5">Junior Software Engineer</h3>
+                <h3 className=" text-3xl font-bold text-center mt-3 lg:mt-5">Software Engineer</h3>
                 <div className=" grid grid-cols-1 text-black dark:text-neutral-200">
                         <h4 className="text-neutral text-lg font-bold">Trackeasy Software Solutions</h4>
                         <p className="text-neutral text-md font-semibold">January-2025 - Present</p>
